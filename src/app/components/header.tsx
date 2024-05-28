d8a4f1816793b25603d80ed95cf8503e199f25d1
sebastianTFRB <sebastian.fajardo@campusucc.edu.co>
2024-05-28 17:37:10 -0500

    cambiesin

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -14,16 +14,16 @@ const Header = () => {
 
   return (
     <header className='flex flex-row items-center justify-between p-4  text-white'>
-      <p className="text-xl font-bold">{t('title')}</p>
+      <p className="text-xl font-bold bg-gradient-to-r from-teal-400 via-teal-600 to-teal-800 bg-clip-text  inline tracking-tight text-transparent">{t('title')}</p>
 
       <label htmlFor="language-select" className="sr-only">
-        {t('select_language')} {/* Agrega una clave para este texto en tu archivo de traducción */}
+        {t('select_language')} 
       </label>
       <select
         id="language-select"
         defaultValue={i18n.language}
         onChange={onChangeLang}
-        className='block px-3 py-2 text-white bg-gray-700 border border-white rounded-lg '
+        className='block px-3 py-2 text-teal-500 bg-black border   shadow-md hover:scale-105 duration-500  mb-6 rounded shadow-teal-500'
       >
         {languageData().map(({ code, label }) => (
           <option key={code} value={code}>
